46859aa080e9f71d29ae6cfd7ffed3f6db48d279
Stuart Pittaway <1201909+stuartpittaway@users.noreply.github.com>
2021-08-16 16:13:49 +0100

    Split influxdb code into separate file

--- ESPController/src/main.cpp
+++ ESPController/src/main.cpp
@@ -59,14 +59,10 @@ const uint8_t diyBMSCurrentMonitorModbusAddress = 90;
 #include <ArduinoJson.h>
 #include "defines.h"
 #include "HAL_ESP32.h"
-
 #include "Rules.h"
-
 #include "avrisp_programmer.h"
-
 #include "tft.h"
-
-#include <HTTPClient.h>
+#include "influxdb.h"
 
 const uart_port_t rs485_uart_num = uart_port_t::UART_NUM_1;
 
@@ -77,9 +73,6 @@ bool _sd_card_installed = false;
 
 //Used for WIFI hostname
 char hostname[16];
-
-HTTPClient *_influx_httpClient = nullptr;
-WiFiClient *_influx_wifiClient = nullptr;
 
 extern bool _tft_screen_available;
 
@@ -1002,6 +995,12 @@ void replyqueue_task(void *param)
       //Small delay to allow watchdog to be fed
       vTaskDelay(pdMS_TO_TICKS(10));
     }
+
+    //Debug - copy module zero to all the other cells for testing
+    for (size_t i = 1; i < TotalNumberOfCells(); i++)
+    {
+      memcpy(&cmi[i], &cmi[0], sizeof(CellModuleInfo));
+    }
   }
 }
 
@@ -1435,170 +1434,17 @@ void connectToMqtt()
   }
 }
 
-static char invalidChars[] = "$&+,/:;=?@ <>#%{}|\\^~[]`";
-
-static char hex_digit(char c)
-{
-  return "0123456789ABCDEF"[c & 0x0F];
-}
-
-String urlEncode(const char *src)
-{
-  int n = 0;
-  char c, *s = (char *)src;
-  while ((c = *s++))
-  {
-    if (strchr(invalidChars, c))
-    {
-      n++;
-    }
-  }
-  String ret;
-  ret.reserve(strlen(src) + 2 * n + 1);
-  s = (char *)src;
-  while ((c = *s++))
-  {
-    if (strchr(invalidChars, c))
-    {
-      ret += '%';
-      ret += hex_digit(c >> 4);
-      ret += hex_digit(c);
-    }
-    else
-      ret += c;
-  }
-  return ret;
-}
-
-static AsyncClient *influx_Client = NULL;
-static int influx_port;
-static String influx_host;
-
-static void influxdb_onData(void *arg, AsyncClient *client, void *data, size_t len)
-{
-  ESP_LOGD(TAG, "Influx data received");
-  //Serial.printf("\n data received from %s \n", client->remoteIP().toString().c_str());
-  //Serial.write((uint8_t*)data, len);
-};
-
-void influxdb_onError(void *arg, AsyncClient *client, err_t error)
-{
-  ESP_LOGE(TAG, "Influx connect error");
-
-  influx_Client = NULL;
-  delete client;
-};
-
-void influxdb_onDisconnect(void *arg, AsyncClient *c)
-{
-  ESP_LOGI(TAG, "Influx disconnected");
-  influx_Client = NULL;
-  delete c;
-};
-
-//Called when the TCP stack connects to INFLUXDB server
-void influxdb_onConnect(void *arg, AsyncClient *client)
-{
-  ESP_LOGD(TAG, "Influx connected");
-
-  String poststring;
-
-  for (uint8_t bank = 0; bank < mysettings.totalNumberOfBanks; bank++)
-  {
-    //TODO: We should send a request per bank not just a single POST as we are likely to exceed capabilities of ESP
-    for (uint8_t i = 0; i < mysettings.totalNumberOfSeriesModules; i++)
-    {
-      if (cmi[i].valid)
-      {
-        //Data in LINE PROTOCOL format https://docs.influxdata.com/influxdb/v2.0/reference/syntax/line-protocol/
-
-        // Example
-        //  myMeasurement,tag1=value1,tag2=value2 fieldKey="fieldValue" 1556813561098000000
-        poststring = poststring + "cells," + "cell=" + String(bank) + "_" + String(i) + " v=" + String((float)cmi[i].voltagemV / 1000.0, 3) + ",i=" + String(cmi[i].internalTemp) + "i" + ",e=" + String(cmi[i].externalTemp) + "i" + ",b=" + (cmi[i].inBypass ? String("true") : String("false")) + "\n";
-      }
-    }
-  }
-
-  String url = String(mysettings.influxdb_serverurl) + String("?org=") + urlEncode(mysettings.influxdb_orgid) + String("&bucket=") + urlEncode(mysettings.influxdb_databasebucket);
-  String token = String("Authorization: Token ") + String(mysettings.influxdb_apitoken);
-
-  //TODO: Need to URLEncode these values
-  String header = "POST " + url + " HTTP/1.1\r\n" + "Host: " + influx_host + "\r\n" + "Connection: close\r\n" + token + "\r\nContent-Length: " + poststring.length() + "\r\n" + "Content-Type: text/plain\r\n" + "\r\n";
-
-  //SERIAL_DEBUG.println(header.c_str());
-  //SERIAL_DEBUG.println(poststring.c_str());
-
-  client->write(header.c_str());
-  client->write(poststring.c_str());
-
-  ESP_LOGD(TAG, "Influx data sent");
-}
-
 void influxdb_task(void *param)
 {
   for (;;)
   {
     //Delay 15 seconds
-    vTaskDelay(pdMS_TO_TICKS(15000));
+    vTaskDelay(pdMS_TO_TICKS(10000));
 
-    if (mysettings.influxdb_enabled && WiFi.isConnected() && rules.invalidModuleCount == 0 && _controller_state == ControllerState::Running)
+    if (mysettings.influxdb_enabled && WiFi.isConnected() && rules.invalidModuleCount == 0 && _controller_state == ControllerState::Running && rules.rule_outcome[Rule::BMSError] == false)
     {
-
-      if (influx_Client == NULL)
-      {
-        //Create new client
-        influx_Client = new AsyncClient();
-
-        String url = String(mysettings.influxdb_serverurl);
-
-        // check for : (http: or https:
-        int index = url.indexOf(':');
-        if (index < 0)
-        {
-          ESP_LOGE(TAG, "Failed to parse protocol");
-        }
-        else
-        {
-          String _protocol = url.substring(0, index);
-
-          influx_port = 80;
-
-          url.remove(0, (index + 3)); // remove http:// or https://
-
-          index = url.indexOf('/');
-          String host = url.substring(0, index);
-          url.remove(0, index); // remove host part
-
-          // get port
-          index = host.indexOf(':');
-          if (index >= 0)
-          {
-            influx_host = host.substring(0, index); // hostname
-            host.remove(0, (index + 1));            // remove hostname + :
-            influx_port = host.toInt();             // get port
-          }
-          else
-          {
-            influx_host = host;
-          }
-
-          ESP_LOGI(TAG, "Influx host %s port %i", influx_host.c_str(), influx_port);
-
-          influx_Client->onData(&influxdb_onData, influx_Client);
-          influx_Client->onConnect(&influxdb_onConnect, influx_Client);
-          influx_Client->onDisconnect(&influxdb_onDisconnect, influx_Client);
-          influx_Client->onError(&influxdb_onError, influx_Client);
-        }
-
-        //Now trigger the connection, and then send the data
-        if (!influx_Client->connect(influx_host.c_str(), influx_port))
-        {
-          ESP_LOGE(TAG, "Influxdb connect fail");
-          AsyncClient *client = influx_Client;
-          influx_Client = NULL;
-          delete client;
-        }
-      }
+      ESP_LOGI(TAG, "Influx task");
+      influx_task_action();
     }
   }
 }
@@ -3402,7 +3248,6 @@ void loop()
       //such as AP reboot, its written to return without action if we are already connected
       connectToWifi();
       wifitimer = currentMillis;
-
       connectToMqtt();
     }
   }
